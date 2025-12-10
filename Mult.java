b3835659d3e95b1703d3fca4f54b7ff400b736e6
WallasM <wallasmadeira@gmail.com>
2025-12-10 23:49:08 +0000

    multiplicação de 10

--- Mult.java
+++ Mult.java
@@ -1,16 +1,16 @@
 import java.util.Scanner;
-public class ContagemRegreciva{
+public class Mult{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("CONTAGEM REGRESSIVA");
         System.out.print( "Digite um numero: ");
         int N = sc.nextInt();
-        for (int i = N; i > 0; i--){
-            System.out.println(i);
+        for (int i = 1; i <= 10;i++){
+            int mult = N * i;
+           System.out.printf("%d * %d = %d\n", N, i, mult); 
+            
         }
 
-    
-        
-
+        sc.close();
     }
 }
