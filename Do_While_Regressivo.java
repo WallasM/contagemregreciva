b7362e8fd72b6f1e26ccc7ddcfdf6a160633443d
WallasM <wallasmadeira@gmail.com>
2025-12-11 00:15:37 +0000

    Do_While

---
+++ Do_While_Regressivo.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Do_While_Regressivo {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Insira o valor maximo da contagem: ");
+        int N = sc.nextInt();
+        int i = N;
+        do{
+            System.out.println(i);
+            i++;
+        } while(i <= 100000000);
+        sc.close(); 
+    }
+}
+
